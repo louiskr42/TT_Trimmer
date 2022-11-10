ee95ab81d425a5f438159ad676afef66fde5e0ec
lkrause <18153@nordakademie.de>
2022-11-10 12:09:09 +0100

    Initial commit

---
+++ app/src/main/java/com/example/tttrimmer/MainActivity.java
@@ -0,0 +1,226 @@
+package com.example.tttrimmer;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.MediaMetadataRetriever;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
+import android.view.View;
+
+import androidx.core.content.ContextCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.tttrimmer.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import java.util.ArrayList;
+
+import com.gowtham.library.utils.LogMessage;
+import com.gowtham.library.utils.TrimType;
+import com.gowtham.library.utils.TrimVideo;
+import droidninja.filepicker.FilePickerBuilder;
+import droidninja.filepicker.FilePickerConst;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Uri> uris = new ArrayList<>();
+    //private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+    public RecyclerView recyclerView;
+    public VideoView videoView;
+    public MediaController mediaController;
+
+    // Register the permissions callback, which handles the user's response to the
+    // system permissions dialog. Save the return value, an instance of
+    // ActivityResultLauncher, as an instance variable.
+    private ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    // Permission is granted. Continue the action or workflow in your
+                    // app.
+                } else {
+                }
+            });
+
+    ActivityResultLauncher<Intent> startForResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK &&
+                        result.getData() != null) {
+                    Uri uri = Uri.parse(TrimVideo.getTrimmedVideoPath(result.getData()));
+                    Log.d(TAG, "Trimmed path:: " + uri);
+                    videoView.setVideoURI(uri);
+
+                } else
+                    LogMessage.v("videoTrimResultLauncher data is null");
+            });
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //setSupportActionBar(binding.toolbar);
+
+        //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        //appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectVideos(view);
+            }
+        });
+
+        recyclerView = findViewById(R.id.recyclerView);
+        mediaController = new MediaController(this);
+        videoView = findViewById(R.id.videoView);
+        videoView.setMediaController(mediaController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //@Override
+    //public boolean onSupportNavigateUp() {
+    //    NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+    //    return NavigationUI.navigateUp(navController, appBarConfiguration)
+    //            || super.onSupportNavigateUp();
+    //}
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //Check condition
+        if (resultCode == RESULT_OK && data != null) {
+            //When activity contains data
+            //Check condition
+            if (requestCode == FilePickerConst.REQUEST_CODE_PHOTO) {
+                //When request for photo
+                //Initialize array list
+                uris = data.getParcelableArrayListExtra(FilePickerConst.KEY_SELECTED_MEDIA);
+                //Set layout manager
+                recyclerView.setLayoutManager(new LinearLayoutManager(this));
+                //Set adapter
+                recyclerView.setAdapter(new MainAdapter(uris));
+                videoView.setVideoURI(uris.get(0));
+
+                MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+                retriever.setDataSource(this, uris.get(0));
+
+                Long oDurationString = toLong(retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION));
+
+                Long nDurationLong = oDurationString - 4;
+
+                TrimVideo.activity(String.valueOf(uris.get(0)))
+                //      .setCompressOption(new CompressOption()) //empty constructor for default compress option
+                        .setHideSeekBar(false)
+                        .setAccurateCut(true)
+                        .setTrimType(TrimType.FIXED_DURATION)
+                        .setFixedDuration(nDurationLong)
+                        .start(this,startForResult);
+
+            }
+        }
+    }
+
+    private Long toLong(String str) {
+        return Long.parseLong(str.substring(0, 2));
+    }
+
+    private void makeToast(int duration) {
+        Toast.makeText(MainActivity.this, duration, Toast.LENGTH_LONG);
+    }
+
+    private void selectVideos(View view) {
+        if (isReadPermissionGranted() && isWritePermissionGranted()) {
+            Snackbar.make(view, "It worked!", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+            openVideoPicker();
+        } else {
+            Snackbar.make(view, "It didn't work!", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+            requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
+            requestPermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+    }
+
+    private void openVideoPicker() {
+        //Open picker
+        FilePickerBuilder.getInstance()
+                .setActivityTitle("Select Videos")
+                .setSpan(FilePickerConst.SPAN_TYPE.FOLDER_SPAN, 3)
+                .setSpan(FilePickerConst.SPAN_TYPE.DETAIL_SPAN, 4)
+                .setMaxCount(1)
+                .setSelectedFiles(uris)
+                .setActivityTheme(R.style.Theme_TTTrimmer_VideoPicker)
+                .enableVideoPicker(true)
+                .enableImagePicker(false)
+                .pickPhoto(this);
+    }
+
+    private boolean isReadPermissionGranted(){
+        if (ContextCompat.checkSelfPermission(
+                MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean isWritePermissionGranted(){
+        if (ContextCompat.checkSelfPermission(
+                MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tttrimmer/MainAdapter.java
@@ -0,0 +1,55 @@
+package com.example.tttrimmer;
+
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
+
+    ArrayList<Uri> uris;
+    MediaController mediaController;
+
+    public MainAdapter(ArrayList<Uri> _uris) {
+        this.uris = _uris;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_main, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        //Print videos using uri
+        holder.imageView.setImageURI(uris.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        //Pass list size
+        return uris.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageView);
+        }
+    }
+}
